93149c737f6df452e9e7e45097e6bc436fd00d16
João Willamy <joao@resolvvi.com>
2020-04-02 21:02:49 -0300

    ✨ improviments

---
+++ src/components/Profile/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Avatar from "../Avatar"
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="Profile-wrapper">
+      <Avatar />
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home</h1>
+    <SEO title="About" />
+    <h1>About Page</h1>
   </Layout>
 )
 
-export default IndexPage
+export default AboutPage
