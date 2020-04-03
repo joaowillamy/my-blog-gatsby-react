d09f76ae5994c55dba8858ba41f921b69fbf9ea5
João Willamy <joao@resolvvi.com>
2020-04-02 23:25:06 -0300

    ✨ Avatar styled

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import GlobalStyles from "../../styles/global"
+import * as S from "./styled"
 import Profile from "../Profile"
-
-import styled from "styled-components"
-
-const LayoutWrapper = styled.section`
-  display: flex;
-`
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
