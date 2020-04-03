c470391d76db9a43472a54323d90f8e59ebac058
João Willamy <joao@resolvvi.com>
2020-04-03 09:06:42 -0300

    🚧 Working in progress

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,15 +3,13 @@ import PropTypes from "prop-types"
 
 import GlobalStyles from "../../styles/global"
 import * as S from "./styled"
-import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
