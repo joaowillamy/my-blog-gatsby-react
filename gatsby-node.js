fd12ed1e0d98f32de7950caaae7c2df8f78475bf
João Willamy <joao@resolvvi.com>
2020-04-05 21:37:23 -0300

    💄 grid and list

--- gatsby-node.js
+++ gatsby-node.js
@@ -77,7 +77,7 @@ exports.createPages = ({ graphql, actions }) => {
       })
     })
 
-    const postsPerPage = 5
+    const postsPerPage = 6
     const numPages = Math.ceil(posts.length / postsPerPage)
 
     Array.from({ length: numPages }).forEach((_, index) => {
--- src/pages/search.js
+++ src/pages/search.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import Search from "../components/Search"
+// import Search from "../components/Search"
 
 const SearchPage = () => (
   <Layout>
