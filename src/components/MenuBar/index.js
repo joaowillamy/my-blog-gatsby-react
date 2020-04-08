c4dc42275b97e9707cd93dc3597c76c51918ce3b
João Willamy <joao@resolvvi.com>
2020-04-08 20:47:42 -0300

    ➕ Add netlify CMS

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 
 import { Home } from "@styled-icons/boxicons-solid/Home"
-import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
+// import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
 import { Bulb as Light } from "@styled-icons/boxicons-regular/Bulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
